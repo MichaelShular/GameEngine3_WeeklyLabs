8ccf8010553c031e308d520c707b49b28e651f45
Mick <mickx.shular@gmail.com>
2021-11-25 21:34:52 -0500

    Change buttons and change AI

--- Assets/Scripts/BattleScene/AICharacter.cs
+++ Assets/Scripts/BattleScene/AICharacter.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 
 public class AICharacter : ICharacter
 {
-    private enum AITypeOfStates { attack, heal, playerStunned};
+    private enum AITypeOfStates { attack, playerStunned, heal, goingForWin};
 
     private AITypeOfStates CurrentState = 0;
     public override void TakeTurn(Encounter encounter)
     {
-        if (encounter.player.incapacitated)
-        {
-            CurrentState = AITypeOfStates.playerStunned;
-        }
-        else if (encounter.enemy.health < 5)
-        {
-            CurrentState = AITypeOfStates.heal;
-        }
-        else
-        {
-            CurrentState = AITypeOfStates.attack;
-        }
 
+        chooseState(encounter);
 
         switch (CurrentState)
         {
@@ -29,18 +18,38 @@ public class AICharacter : ICharacter
                 UseAbilty(percentOfAbility(50, 51, 75), this, encounter.player);
                 break;
             case (AITypeOfStates)1:
-                UseAbilty(percentOfAbility(20, 70, 85), this, encounter.player);
+                UseAbilty(percentOfAbility(60, 70, 100), this, encounter.player);
                 break;
             case (AITypeOfStates)2:
-                UseAbilty(percentOfAbility(60, 70, 100), this, encounter.player);
+                UseAbilty(percentOfAbility(10, 70, 85), this, encounter.player);
+                break;
+            case (AITypeOfStates)3:
+                UseAbilty(percentOfAbility(100, 101, 101), this, encounter.player);
                 break;
             default:
                 break;
         }
-
-        //Debug.Log("AI Turn");
     }
 
+    private void chooseState(Encounter encounter)
+    {
+        if (encounter.player.incapacitated)
+        {
+            CurrentState = AITypeOfStates.playerStunned;
+            return;
+        }
+        if(encounter.player.health < 5)
+        {
+            CurrentState = AITypeOfStates.goingForWin;
+            return;
+        }
+        if (encounter.enemy.health <= 10)
+        {
+            CurrentState = AITypeOfStates.heal;
+            return;
+        }
+        CurrentState = AITypeOfStates.attack;
+    }
     private int percentOfAbility(int rangeForSlot0, int rangeForSlot1, int rangeForSlot2)
     {
         int abilityToUse = Random.Range(0, 100);
